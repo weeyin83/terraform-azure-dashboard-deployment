775504c5a8bf5d6cda68b84d39fcb5f50fdd2fb3
Sarah Lean <Sarah.Lean@microsoft.com>
2023-10-19 10:50:59 +0100

    update terraform and dashboard files

--- main.tf
+++ main.tf
@@ -7,29 +7,24 @@ module "dashboard" {
   source  = "kumarvna/dashboard/azurerm"
   version = "1.1.0"
 
-  # By default, this module will not create a resource group. Location will be same as existing RG.
-  # proivde a name to use an existing resource group, specify the existing resource group name, 
-  # set the argument to `create_resource_group = true` to create new resrouce group.
-  resource_group_name = "rg-shared-westeurope-01"
+  # This module will not create a resource group, provide the name and location of an existing resource group.
+  resource_group_name = "demo-azurelab"
   location            = "westeurope"
 
   # Dashboard configuration
   dashboards = [
     {
-      name           = "my-cool-dashboard"
-      json_file_path = "./test_dashboard.json"
+      name           = "Azure-Arc-Windows-Linux-Dashboard"
+      json_file_path = "./dashboard.json"
       variables = {
-        "title" = "example-dashboard"
+        "title" = "Azure-Arc-Windows-Linux-Dashboard"
       }
     }
   ]
 
   # Adding TAG's to your Azure resources 
   tags = {
-    ProjectName  = "demo-internal"
-    Env          = "dev"
-    Owner        = "user@example.com"
-    BusinessUnit = "CORP"
-    ServiceClass = "Gold"
+    ProjectName  = "azurearc"
+    Env          = "prod"
   }
 }
